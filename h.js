71a0c81ecaea809a29d5d5ae9706fb1036a23f24
wilian1808 <wilian.walli@gmail.com>
2021-03-12 16:45:40 -0500

    delete h.js

--- h.js
+++
@@ -1,8 +0,0 @@
-db.geekFlareCollection.updateOne({"product" : "bottles"}, {$set : {"Qty": 40}})
-
-db.users.update({"username": "pedro"}, {$set: {"age": 55}})
-
-console.log("hola");
-
-// delete
-db.users.deleteMany({ username: "mario" })
